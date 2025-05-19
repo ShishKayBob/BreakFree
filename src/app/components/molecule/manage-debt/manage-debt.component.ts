7899b497560dd19afac55722afc36418bb9ba4bf
Kay Olecki <keolecki@gmail.com>
2025-05-18 23:44:48 -0700

    Working table that can display new debts

--- src/app/components/molecule/manage-debt/manage-debt.component.ts
+++ src/app/components/molecule/manage-debt/manage-debt.component.ts
@@ -1,13 +1,46 @@
 import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { TableModule } from 'primeng/table';
+import Debt from '../../../types/debt';
+import { DebtService } from '../../../services/debt.service';
+import { Subscription } from 'rxjs';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'manage-debt',
-  imports: [CardModule, TableModule],
+  imports: [CardModule, TableModule, ButtonModule],
   templateUrl: './manage-debt.component.html',
   styleUrl: './manage-debt.component.scss'
 })
 export class ManageDebtComponent {
+  public debts: Debt[] = [];
+
+  private debtServiceSubscription!: Subscription;
+
+  constructor (private debtService: DebtService) { }
+
+  ngOnInit() {
+    this.debtServiceSubscription = this.debtService.$debtService
+      .subscribe((value: Debt[]) => {
+        this.debts = value;
+      });
+  }
+
+  ngOnDestroy() {
+    this.debtServiceSubscription?.unsubscribe();
+  }
+
+  public addDebt() {
+    const debt: Debt = {
+      name: 'amex',
+      type: 'credit',
+      initialBalance: 15000,
+      currentBalance: 5000,
+      interestRate: 0.25,
+      estMinPayment: 450,
+      paymentHistory: [],
+    };
+    this.debtService.addDebt(debt);
+  }
 
 }
--- src/app/services/debt.service.ts
+++ src/app/services/debt.service.ts
@@ -1,28 +1,40 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import Debt from '../types/debt';
+import Payment from '../types/payment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DebtService {
-
-  public $debtService!: BehaviorSubject<any>;
+  public $debtService!: BehaviorSubject<Debt[]>;
 
   private debts: Debt[] = [];
 
-  constructor() { }
+  constructor() { 
+    this.$debtService = new BehaviorSubject(this.debts);
+  }
 
-  public addDebt() {
+  public addDebt(debt: Debt) {
+    this.debts.push(debt);
+    this.$debtService.next(this.debts);
+    console.log(debt);
 
   }
 
-  public removeDebt() {
-
+  public editDebt(debtIndex: number, debt: Debt) {
+    this.debts[debtIndex] = debt;
+    this.$debtService.next(this.debts);
   }
 
-  public makePayment() {
-    
+  public removeDebt(debtIndex: number) {
+    this.debts.splice(debtIndex, 1);
+    this.$debtService.next(this.debts);
+  }
+
+  public makePayment(debtIndex: number, payment: Payment) {
+    this.debts[debtIndex].paymentHistory.push(payment);
+    this.$debtService.next(this.debts);
   }
 
 }
