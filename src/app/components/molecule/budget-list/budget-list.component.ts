f1f74db6640c5bb4aab45c682e60d88d5a347a8b
Kay Olecki <keolecki@gmail.com>
2025-05-21 09:19:47 -0700

    Started configurable table

--- src/app/components/molecule/budget-list/budget-list.component.ts
+++ src/app/components/molecule/budget-list/budget-list.component.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { InputTextModule } from 'primeng/inputtext';
 import { TableModule } from 'primeng/table';
+import Budget from '../../../types/budget';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'budget-list',
@@ -12,11 +16,16 @@ import { TableModule } from 'primeng/table';
     InputTextModule,
     InputNumberModule,
     ButtonModule,
-    ReactiveFormsModule,
-    CommonModule],
+    FormsModule,
+    CommonModule,
+    InputGroupAddonModule,
+    InputGroupModule,
+    CardModule],
   templateUrl: './budget-list.component.html',
   styleUrl: './budget-list.component.scss'
 })
 export class BudgetListComponent {
+
+  public budget: Budget = { takeHome: 0, categories: []};
 
 }
